ea3b898e293f2e6d117d2da5eec27791199b548d
Lee Chengkai <leechengkai@gmail.com>
2025-11-15 22:44:22 +0800

    Solve quest 10 part 3

--- quest10/part3.py
+++ quest10/part3.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def main():
     with open("quest10/data/input3.txt", "r") as file:
         lines = file.readlines()
@@ -20,117 +23,137 @@ def main():
                 elif char == "#":
                     hideouts.add((r, c))
 
-    game_state = {
-        "dragon": dragon_pos,
-        "sheep": sheep_pos,
-        "moves": list(),
-        "debug": "",
-    }
+        game_states = [(dragon_pos, tuple(sorted(sheep_pos)))]
+        visited = defaultdict(list)
+        visited[game_states[0]].append((None, None, None))
 
-    dragon_win_count = 0
-    game_states = [game_state]
-    # visited = set()
-    # Need to track number of ways to reach a state
-    visited = dict()
-    while game_states:
-        next_states = []
-        for game_state in game_states:
-            for after_sheep in sheep_turn(game_state, hideouts, r_max, c_max):
-                if after_sheep is None:
-                    # Sheep wins
-                    pass
-                    # print("Sheep wins. Moves:" + " ".join(game_state["moves"]))
-                else:
-                    for after_dragon in dragon_turn(
-                        after_sheep, hideouts, r_max, c_max
-                    ):
-                        if len(after_dragon["sheep"]) == 0:
-                            # Dragon wins
-                            dragon_win_count += 1
-                            print(
-                                f"Dragon win #{dragon_win_count}. Moves:"
-                                + " ".join(after_dragon["moves"])
+        while game_states:
+            next_data = []
+            for game_state in game_states:
+                for after_sheep, sheep_move in sheep_turn(
+                    game_state, hideouts, r_max, c_max
+                ):
+                    if after_sheep is None:
+                        # Sheep wins
+                        pass
+                    else:
+                        for after_dragon, dragon_move in dragon_turn(
+                            after_sheep, hideouts, r_max, c_max
+                        ):
+                            next_data.append(
+                                (after_dragon, sheep_move, dragon_move, game_state)
                             )
-                        else:
-                            k = state_key(after_dragon)
 
-                            visited.add(state_key(after_dragon))
-                            next_states.append(after_dragon)
+            game_states = []
+            for state, sheep_move, dragon_move, parent in next_data:
+                if state not in visited and len(state[1]) > 0:
+                    game_states.append(state)
 
-        print("next states:", len(next_states))
-        game_states = next_states
+                visited[state].append((parent, sheep_move, dragon_move))
+            print("States to explore:", len(game_states))
 
-    print("Dragon win count:", dragon_win_count)
+        # rebuild_winning_sequences(visited)
+        count_winning_sequences(visited)
+
+
+def rebuild_winning_sequences(visited):
+    dragon_winning_sequences = []
+    frontier = []
+
+    dragon_win_states = [state for state in visited.keys() if len(state[1]) == 0]
+    for state in dragon_win_states:
+        frontier.append((state, ""))
+
+    while frontier:
+        current_key, sequence = frontier.pop()
+        parents = visited[current_key]
+        for parent_key, sheep_move, dragon_move in parents:
+            if parent_key is None:
+                # Reached root
+                dragon_winning_sequences.append(sequence.strip())
+            else:
+                new_sequence = f"{sheep_move} {dragon_move} " + sequence
+                frontier.append((parent_key, new_sequence))
+
+    if len(dragon_winning_sequences) < 50:
+        for seq in dragon_winning_sequences:
+            print(seq)
+
+    print("Number of winning sequences:", len(dragon_winning_sequences))
+
+
+def count_winning_sequences(visited):
+    count_ways = {}
+
+    def count_ways_to_win(state_key):
+        if state_key in count_ways:
+            return count_ways[state_key]
+
+        parents = visited[state_key]
+        total_ways = 0
+        for parent_key, sheep_move, dragon_move in parents:
+            if parent_key is None:
+                total_ways += 1
+            else:
+                total_ways += count_ways_to_win(parent_key)
+
+        count_ways[state_key] = total_ways
+        return total_ways
+
+    total_dragon_winning_sequences = 0
+    dragon_win_states = [state for state in visited.keys() if len(state[1]) == 0]
+    for state in dragon_win_states:
+        total_dragon_winning_sequences += count_ways_to_win(state)
+
+    print("Number of winning sequences:", total_dragon_winning_sequences)
 
 
 def sheep_turn(game_state, hideouts, r_max, c_max):
     has_valid_moves = False
-    for sheep_pos in game_state["sheep"]:
+    dragon = game_state[0]
+    sheep = game_state[1]
+
+    for sheep_pos in sheep:
         new_r = sheep_pos[0] + 1
 
         if new_r >= r_max:
             # Sheep wins
-            yield None
+            yield None, None
             return
 
         new_pos = (new_r, sheep_pos[1])
-        if new_pos != game_state["dragon"] or new_pos in hideouts:
+        if new_pos != dragon or new_pos in hideouts:
             has_valid_moves = True
-            move_id_str = move_id("S", new_pos)
-            # next_sheep = game_state["sheep"] - {sheep_pos} + {new_pos}
-            next_sheep = set(game_state["sheep"])
+
+            next_sheep = set(sheep)
             next_sheep.remove(sheep_pos)
             next_sheep.add(new_pos)
 
-            yield {
-                "dragon": game_state["dragon"],
-                "sheep": next_sheep,
-                "moves": game_state["moves"] + [move_id_str],
-            }
+            yield (dragon, tuple(sorted(next_sheep))), move_id("S", new_pos)
 
     if not has_valid_moves:
         # Sheep cannot move, skips turn
-        yield {
-            "dragon": game_state["dragon"],
-            "sheep": game_state["sheep"],
-            "moves": game_state["moves"] + ["S>SKIP"],
-        }
+        yield (dragon, tuple(sorted(sheep))), "S>"
 
 
 def dragon_turn(game_state, hideouts, r_max, c_max):
-    for dragon_pos in generate_moves(game_state["dragon"], r_max, c_max):
-        if dragon_pos in game_state["sheep"] and dragon_pos not in hideouts:
-            next_sheep = set(game_state["sheep"])
+    dragon = game_state[0]
+    sheep = game_state[1]
+
+    for dragon_pos in generate_moves(dragon, r_max, c_max):
+        if dragon_pos in sheep and dragon_pos not in hideouts:
+            next_sheep = set(sheep)
             next_sheep.remove(dragon_pos)
-            next_state = {
-                "dragon": dragon_pos,
-                "sheep": next_sheep,
-                "moves": game_state["moves"] + [move_id("D", dragon_pos)],
-            }
-            yield next_state
+            next_state = (dragon_pos, tuple(sorted(next_sheep)))
+            yield next_state, move_id("D", dragon_pos)
         else:
-            yield {
-                "dragon": dragon_pos,
-                "sheep": game_state["sheep"],
-                "moves": game_state["moves"] + [move_id("D", dragon_pos)],
-            }
+            yield (dragon_pos, sheep), move_id("D", dragon_pos)
 
 
 def move_id(player, pos):
     row, col = pos
     letter = chr(ord("A") + col)
     return f"{player}>{letter}{row + 1}"
-
-
-def state_key(state) -> tuple:
-    return (state["dragon"], *tuple(sorted(state["sheep"])))
-    # return (len(state["moves"]), state["dragon"], *tuple(sorted(state["sheep"])))
-
-
-def serialize_state(dragon, sheep) -> str:
-    dragon_str = f"D:{dragon[0]},{dragon[1]}"
-    sheep_str = " ".join(f"{s[0]},{s[1]}" for s in sorted(sheep))
-    return f"{dragon_str}|{sheep_str}"
 
 
 def generate_moves(pos, r_max, c_max):
